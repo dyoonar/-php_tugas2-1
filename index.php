9855ec0013312689c6a5824a29f76036fbe79461
dyoonar <71246827+dyoonar@users.noreply.github.com>
2020-10-07 11:57:08 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$a = 36;
+$b = 12;
+$c = 24;
+$x = $a / $b * $c;
+
+printf ($a." Hari = ".$b." Pekerja <br>");
+printf ("X Hari = ".$c." Pekerja <br>");
+printf ("X = ...? <br>");
+printf ("Jawaban = ".$x);
+?>
